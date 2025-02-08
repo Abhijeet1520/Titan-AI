2579f8c3c5a5473cf919de6bbb6eda5ad6952b69
Abhijeet Kumar <abhijeetkumar12346@gmail.com>
2025-02-08 16:23:51 +0000

    add server setup and test API script for chatbot functionality

---
+++ aiagent/cdp-chatbot/server.ts
@@ -0,0 +1,10 @@
+import { app } from "./chatbot";
+
+/***************************************************
+ * Start server
+ ***************************************************/
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
